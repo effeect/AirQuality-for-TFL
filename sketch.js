ddbb3503b8a5abb7e0110b43a4b65933585e2343
Oliver Dimes <oliver.dimes3@gmail.com>
2019-10-01 14:44:10 +0100

    Adding in arrays
    Almost there just need to filter out data and we can start sketching.

--- sketch.js
+++ sketch.js
@@ -16,6 +16,9 @@ let groupsJSON;
 //Arrays for storing the objects from JSON so we can modify and utlise the data if need be
 let groups = [];
 let AirQuality = [];
+let viewInfoArray = [];
+
+let airQualityIndexResults = [];
 
 function preload(){ //Preloading only contains JSON related function. We need to make sure it's ready before the sketch begins
     
@@ -38,14 +41,25 @@ function setup(){
     
     for(var i = 0; i < groupList; i++)
         {
-            groups[i] = groupsJSON.Groups.Group[i]
+            groups[i] = groupsJSON.Groups.Group[i] //Gives me the group name
             console.log(groups)
             
             let groupNameConcat = "@GroupName"
             let GroupNameTemp = groups[i][groupNameConcat] //THIS WORKS!
-            console.log(GroupNameTemp)
+            console.log("@" + GroupNameTemp)
             
             AirQuality = hourlyJSON.HourlyAirQualityIndex[GroupNameTemp]
+            
+            console.log(AirQuality)
+            let changingURL = "http://api.erg.kcl.ac.uk/AirQuality/Hourly/MonitoringIndex/GroupName="+GroupNameTemp+"/Json"
+            
+            viewInfoArray[i] = loadJSON(changingURL) //Puts the group names into an array of objects where we can mantiuplate the data
+            
+            console.log(viewInfoArray)
+            
+            //Find the length of the species, site and local authority and throw that into the mix
+            //[8].HourlyAirQualityIndex.LocalAuthority[""0""].Site[""0""].Species[""0""][""@AirQualityIndex""]
+            
         }
 }
 
